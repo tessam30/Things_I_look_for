f4de57028044f25c6cea68fa4d2550dfaa6fcc5a
tessam30 <tessam30@users.noreply.github.com>
2018-11-03 23:01:18 -0400

    Update lookfor.R

--- lookfor.R
+++ lookfor.R
@@ -1,5 +1,9 @@
 # Random snippets of code I'm looking for at various times -- consolidated in a single place.
 
+# Useful resources: 
+# https://www.listendata.com/2016/08/dplyr-tutorial.html
+# 
+
 # Akin to Stata's isid command inside dplyr
 # Checking for uniqueness using var1 and var2
 x2 = distinct(df, var1, var2, .keep_all= TRUE)
@@ -10,6 +14,27 @@ select(df, -var1, var2)
 drop_vars <- c("var1", "var2")
 select(df, -one_of())
 
+#Reorder variables in a data frame
+df2 <- select(df, var1, var2, everything())
+
+# rename syntax
+rename(data , new_name = old_name)
+
+# filter with negations
+mydata10 = filter(df, !var1 %in% c("A", "C"))
+
+# using grepl - look for records where the State varaible contains "Ar"
+mydata10 = filter(df, grepl("Ar", State))
+
+# Summarise over variables - creating 3 new calcuations for each variable
+summarise_at(df, vars(var1, var2), funs(n(), mean, median))
+
+# Convert values to na
+k <- c("a", "b", "", "d")
+na_if(k, "")
+
+# Print a list of names to the screen in a 
+dput(names(shocks))
 
 
 
