7fd943b0cc597e37739e66cce9e05085e5215c65
tessam30 <tessam30@users.noreply.github.com>
2019-11-27 14:50:33 -0500

    Update lookfor.R

--- lookfor.R
+++ lookfor.R
@@ -357,6 +357,40 @@ map2(file.path(imagepath, paste0("Category ", plots$Category,
      dpi = 300, 
      ggsave)
 
+ ################# Options for plots in a function ###########################     
+ #Create a function that gives back a bar or map plot, depending on input
+ msme_plot <- function(df, x, option = 1) { 
+  if (!option %in% c(1, 2)) {
+    stop("Select 1 (map) or 2 (graph) as option value.")
+  }
+  
+  xvar = enquo(x)
+  
+  if (option == 1) {
+      ggplot(df) +
+      geom_sf(aes(fill = !!xvar), colour = "white") +
+      scale_fill_viridis_c(option = "C", direction = -1, alpha = 0.90) +
+      theme_minimal() +
+      labs(caption = "GeoCenter Calculations from MSME 2016 Report",
+           title = (gsub("`", "", {rlang::quo_text(xvar)}))) 
+  }
+  
+ else if (option == 2) {
+    df %>% 
+      mutate(sortvar = fct_reorder(County, !!xvar), .desc = TRUE) %>% 
+      ggplot(aes(x = sortvar, y = !!xvar)) +
+      geom_col() +
+      coord_flip() + theme_minimal() +
+      labs(caption = "GeoCenter Calculations from MSME 2016 Report",
+           title = (gsub("`", "", {rlang::quo_text(xvar)})))
+   }
+}
+
+
+msme_plot(msme_geo, `Table 3_1 Sampled Licensed Establishments`, option = 1)
+      
+      
+      
 
 #-------------------------------- Plot specific -----------------------------
 
